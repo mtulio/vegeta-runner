c18a0595ebd35799260cfdd11e3e8d55410293b9
Marco Tulio R Braga <marco.tulio@mtulio.eng.br>
2018-07-27 19:11:18 -0300

    Add support to create reports - start point

--- runner.sh
+++ runner.sh
@@ -11,7 +11,11 @@ set +x
 PLAN_FILE=$1
 VEGETA_EXTRA_ARGS="-keepalive -http2=f -header=\"Content-type:gzip\""
 VEGETA_BIN=${3:-./bin/vegeta}
+VEGETA_OUTPUT_REPORTS=()
 
+#########################
+# Vegeta Plan
+#########################
 function run_plan(){
   PLAN_RATE=$1
   PLAN_NAME_LABEL=${PLAN_NAME}-${PLAN_TEST_DURATION}-${PLAN_RATE}rps
@@ -20,7 +24,7 @@ function run_plan(){
   ${VEGETA_BIN} attack \
     -keepalive \
     -http2=f \
-    -header=\"Content-type:gzip\" \
+    -header="User-Agent:vegeta-${PLAN_NAME_LABEL}" \
     -targets=input/${PLAN_INPUT} \
     -duration=${PLAN_TEST_DURATION} \
     -rate=${PLAN_RATE} \
@@ -28,6 +32,7 @@ function run_plan(){
   set +x
   if [[ -f output/${PLAN_NAME_LABEL}.bin ]]; then
     echo "Results are saved on file: output/${PLAN_NAME_LABEL}.bin"
+    VEGETA_OUTPUT_REPORTS+=output/${PLAN_NAME_LABEL}.bin
   fi
 }
 
@@ -42,9 +47,39 @@ function setup_plan(){
   IFS=','
   for T_RATE in $PLAN_TEST_RATES; do
     run_plan $T_RATE
-  done
 
+  done
+  #run_report $PLAN_NAME
 }
+
+#########################
+# Vegeta Report
+#########################
+
+function run_report(){
+  # Sample to run
+  #./bin/vegeta report \
+  #  -inputs=output/test-azure-60s-17rps.bin,output/test-azure-60s-36rps.bin \
+  #  -reporter=plot > output/test-azure.html
+
+  echo "run_report()"
+
+  OUT_FILES=""
+  echo $OUT_FILES
+  echo ${VEGETA_OUTPUT_REPORTS[@]}
+  for i in ${VEGETA_OUTPUT_REPORTS[@]}; do
+    echo $OUT_FILES
+    OUT_FILES="$OUT_FILES,$i"
+  done
+  ${VEGETA_BIN} report -inputs=${OUT_FILES} -reporter=json > output/${PLAN_NAME}.json
+  ${VEGETA_BIN} report -inputs=${OUT_FILES} -reporter=txt > output/${PLAN_NAME}.txt
+  ${VEGETA_BIN} report -inputs=${OUT_FILES} -reporter=plt > output/${PLAN_NAME}.plt
+}
+
+
+#########################
+# Main
+#########################
 
 echo "# Parsing plan file $PLAN_FILE"
 while read line; do
